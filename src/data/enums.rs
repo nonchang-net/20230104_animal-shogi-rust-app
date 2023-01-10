6b62b63458ebba20da929eeeda89bae8feabb04e
nonchang <n_o_n_t_a_2_0_1_4@yahoo.co.jp>
2023-01-10 20:00:45 +0900

    initial commit: rustの書き方を検討中

---
+++ src/data/enums.rs
@@ -0,0 +1,20 @@
+// #[allow(unused_variables)]
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum Side{
+    Free,
+    A,
+    B
+}
+
+// #[allow(unused_variables)]
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum Koma{
+    Null,
+    Lion,
+    Kirin,
+    Zou,
+    Hiyoko,
+    Niwatori
+}
---
+++ src/data/types.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+use crate::data::enums::{
+    Koma,
+    Side
+};
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Cell{
+    pub side: Side,
+    pub koma: Koma,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Board{
+    // 盤情報の二次元配列
+    pub data: [[Cell; 3]; 4],
+
+    // それぞれのサイドの手駒sideをキーにしたKomaの配列
+    pub tegomas: HashMap<Side, Vec<Koma>>
+}
+
+#[allow(dead_code)]
+impl Board{
+    fn area(&self) -> u32 {
+        // self.width * self.height
+        return 123;
+    }
+}
+
+// structは初期値とかは作れない……？ テスト
+// pub struct BT1{
+//     pub test1: [[Cell]],
+//     pub test: [[Cell]] = [[Cell{side:Side.A}, Cell{side:Side.B}]],
+// }
