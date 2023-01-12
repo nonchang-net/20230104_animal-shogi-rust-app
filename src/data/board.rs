62621ee4a444cddaead436a23e62c5dc6f91fefb
nonchang <n_o_n_t_a_2_0_1_4@yahoo.co.jp>
2023-01-13 00:17:02 +0900

    TODO: error[E0063]: missing field `arrackable_maps` in initializer of `Board`

--- src/data/board.rs
+++ src/data/board.rs
@@ -19,8 +19,12 @@ pub struct Board{
 	// 盤情報の二次元配列
 	pub data: [[Cell; 3]; 4],
 
-	// それぞれのサイドの手駒sideをキーにしたKomaの配列
-	pub tegomas: HashMap<Side, Vec<Koma>>
+	// 手駒: sideをキーにしたKomaの配列
+	pub tegomas: HashMap<Side, Vec<Koma>>,
+
+	// 効いてる場所の一覧
+	// TODO: なんかここでエラー出ている
+	// pub arrackable_maps: HashMap<Side, FlagBoard>,
 }
 
 #[allow(dead_code)]
@@ -29,10 +33,16 @@ impl Board{
 		// self.width * self.height
 		return 123;
 	}
+
+	fn get_attackable_map(side:Side) -> FlagBoard{
+		// TODO: arrackable_mapsから取得できなければ生成、あれば取得してreturnする
+		let mut attackable_map = FlagBoard::new(false);
+		return attackable_map
+	}
 }
 
 // 盤状態のフラグマップを表現する型
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FlagBoard{
     pub data: [[bool; 3] ;4]
 }
